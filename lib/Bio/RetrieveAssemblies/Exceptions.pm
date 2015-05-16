57a1f25e69cffb297a0625d4594caca3c354c36c
andrewjpage <andrewjpage@gmail.com>
2015-05-17 00:48:04 +0100

    download accession files

--- lib/Bio/RetrieveAssemblies/Exceptions.pm
+++ lib/Bio/RetrieveAssemblies/Exceptions.pm
@@ -10,7 +10,8 @@ Exceptions for input data
 
 use Exception::Class (
     Bio::RetrieveAssemblies::Exceptions::CouldntDownload => { description => 'Couldnt download RefWeak' },
-    Bio::RetrieveAssemblies::Exceptions::CSVParser              => { description => 'CSV parser error' },
+    Bio::RetrieveAssemblies::Exceptions::CSVParser       => { description => 'TSV parser error' },
+    Bio::RetrieveAssemblies::Exceptions::GenBankToGFFConverter => { description => 'Couldnt convert GenBank file to GFF file' },
 );
 
 1;
